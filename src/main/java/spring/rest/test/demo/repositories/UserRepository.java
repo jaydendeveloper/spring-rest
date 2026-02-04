6398bd165993b885cf957f78f881a103753b9a85
jaydendeveloper <contact.jaydendev@gmail.com>
2026-02-04 10:58:52 +0100

    feat: Add 'add' and 'delete' methods to User Repository

--- src/main/java/spring/rest/test/demo/repositories/UserRepository.java
+++ src/main/java/spring/rest/test/demo/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package spring.rest.test.demo.repositories;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
 import spring.rest.test.demo.models.User;
 
 @Repository
@@ -12,20 +14,32 @@ public class UserRepository {
 
 
     public UserRepository(){
-        users.add(new User("John", "Doe", "john.doe@example.com", "password123"));
-        users.add(new User("Jane", "Doe", "jane.doe@example.com", "password456"));
-        users.add(new User("Jim", "Beam", "jim.beam@example.com", "password789"));
-        users.add(new User("Jack", "Daniels", "jack.daniels@example.com", "password101"));
-        users.add(new User("Jill", "Valentine", "jill.valentine@example.com", "password102"));
-        users.add(new User("James", "Bond", "james.bond@example.com", "password103"));
-        users.add(new User("Jessica", "Jones", "jessica.jones@example.com", "password104"));
+        users.add(new User(1,"John", "Doe", "john.doe@example.com", "password123"));
+        users.add(new User(2,"Jane", "Doe", "jane.doe@example.com", "password456"));
+        users.add(new User(3,"Jim", "Beam", "jim.beam@example.com", "password789"));
+        users.add(new User(4,"Jack", "Daniels", "jack.daniels@example.com", "password101"));
     }
 
     public List<User> getUsers() {
         return users;
     }
 
+    public int getNextId() {
+        if (users.isEmpty()) {
+            return 1;
+        }
+        return users.get(users.size() - 1).getId() + 1;
+    }
+
     public void setUsers(List<User> users) {
         this.users = users;
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public void deleteUser(int id){
+        this.users = this.users.stream().filter(u -> u.getId() != id).toList();
     }
 }
